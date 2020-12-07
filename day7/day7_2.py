863a8735b96991303726553bfea3e7f7fe3a0eba
novellep <alberto109@hotmail.es>
2020-12-07 11:15:10 +0100

    day 7 part 2

--- day7/day7_2.py
+++ day7/day7_2.py
@@ -1,12 +1,19 @@
 import re
 
 
-def count_node_childrens_with_weight(node_name, prev_weight, visited_nodes, graph, result):
-    pass
+def count_node_childrens_with_weight(node_name, visited_nodes, graph):
+    acc = 0
+    for curr_node, curr_weight in graph.get(node_name, []):
+        if curr_node not in visited_nodes:
+            acc += curr_weight + \
+                   curr_weight * \
+                   count_node_childrens_with_weight(curr_node, {*visited_nodes, curr_node}, graph)
+    return acc
+
 
 if __name__ == '__main__':
     graph = {}
-    with open('test_input', 'r') as f:
+    with open('input', 'r') as f:
         for line in f.readlines():
             container_info, content_info = line.split('contain')
             if content_info.strip() == 'no other bags.':
@@ -18,8 +25,4 @@ if __name__ == '__main__':
                 match = re.search('([0-9]+)(?: )([a-z][a-z ]*(?= bags?))', inner_bag_info)
                 amount, color = int(match.group(1)), match.group(2)
                 graph[container_color].append((color, amount))
-    import pprint
-    pprint.pprint(graph)
-    result = [0]
-    print(count_node_childrens_with_weight('shiny gold', 1, set('shiny gold'), graph, result))
-    print(result)
+    print(count_node_childrens_with_weight('shiny gold', set(''), graph))
