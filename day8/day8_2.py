4e1a2934e140a0108c17149a5bb9b8dbdf6d5a47
NovelleP <alberto109@hotmail.es>
2020-12-08 14:57:01 +0100

    day 8 part 2: refactoring and adding recursive solution

--- day8/day8_2.py
+++ day8/day8_2.py
@@ -12,21 +12,45 @@ def jmp(arg, pc):
 
 
 def change_instruction(instruction, instruction_mapping):
-    return instruction_mapping[instruction[0]], instruction[1]
+    return instruction_mapping.get(instruction[0], instruction[0]), instruction[1]
+
+
+def exec_instruction(op, arg, accumulator, pc):
+    if op == 'acc':
+        accumulator = acc(arg, accumulator)
+        pc += 1
+    elif op == 'jmp':
+        pc = jmp(arg, pc)
+    else:
+        pc += 1
+    return pc, accumulator
 
 
 def run_without_loops(instructions, accumulator, pc, executed):
     while (pc < len(instructions)) and (not executed[pc]):
         executed[pc] = True
         op, arg = instructions[pc]
-        if op == 'acc':
-            accumulator = acc(arg, accumulator)
-            pc += 1
-        elif op == 'jmp':
-            pc = jmp(arg, pc)
-        else:
-            pc += 1
+        pc, accumulator = exec_instruction(op, arg, accumulator, pc)
     return pc, accumulator
+
+
+def run_without_loops_v2(instructions, accumulator, pc, executed, flag, instruction_ampping):
+    if pc == len(instructions):
+        print(accumulator)
+        flag[0] = True
+        return
+
+    if not flag[0]:
+        if not executed[pc]:
+            executed[pc] = True
+            op, arg = instructions[pc]
+            tmp_pc, tmp_accumulator = exec_instruction(op, arg, accumulator, pc)
+            run_without_loops_v2(instructions, tmp_accumulator, tmp_pc, executed, flag, instruction_ampping)
+            if op in instruction_ampping and not flag[0]:
+                op, arg = change_instruction(instructions[pc], instruction_ampping)
+                tmp_pc, tmp_accumulator = exec_instruction(op, arg, accumulator, pc)
+                run_without_loops_v2(instructions, tmp_accumulator, tmp_pc, executed, flag, instruction_ampping)
+
 
 
 if __name__ == '__main__':
@@ -37,6 +61,8 @@ if __name__ == '__main__':
         'jmp': 'nop',
         'nop': 'jmp'
     }
+    import time
+    start = time.perf_counter_ns()
     instructions_to_change_idx = (idx for idx, (op, _) in enumerate(instructions) if op in instruction_ampping)
     for idx in instructions_to_change_idx:
         new_instruction = change_instruction(instructions[idx], instruction_ampping)
@@ -45,4 +71,9 @@ if __name__ == '__main__':
         if pc == len(instructions):
             print(accumulator)
             break
+    print(f'Tiempo (ns) solucion iterativa: {time.perf_counter_ns() - start}')
+
+    start = time.perf_counter_ns()
+    run_without_loops_v2(instructions, 0, 0, [False for _ in instructions], [False], instruction_ampping)
+    print(f'Tiempo (ns) solucion recursiva: {time.perf_counter_ns() - start}')
 
