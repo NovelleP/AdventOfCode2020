38f15c34eecb3875d747892758a03ff31c8c7d4f
novellep <alberto109@hotmail.es>
2020-12-18 14:41:36 +0100

    Day 18

---
+++ day18/day18_2.py
@@ -0,0 +1,32 @@
+class Integer:
+
+    def __init__(self, n):
+        self.n = n
+
+    def __add__(self, integer):
+        return Integer(self.n * integer.n)
+
+    def __mul__(self, integer):
+        return Integer(self.n + integer.n)
+
+    def __str__(self):
+        return str(self.n)
+
+
+def is_int(val):
+    try:
+        int(val)
+        return True
+    except:
+        return False
+
+
+def change_sign(val):
+    return '+' if val == '*' else '*' if val == '+' else val
+
+
+if __name__ == '__main__':
+    with open('input', 'r') as f:
+        exprs = [''.join(f'Integer({val})' if is_int(val) else change_sign(val) for val in line) for line in f.read().strip().splitlines()]
+
+    print(sum(eval(expr).n for expr in exprs))
